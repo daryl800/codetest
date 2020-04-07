9127bc578e3be64438afda0405ed35c40559668f
Daryl <ng.daryl@gmail.com>
2020-04-07 08:49:47 +0200

    Initial Commit

--- src/main/java/se/kry/codetest/BackgroundPoller.java
+++ src/main/java/se/kry/codetest/BackgroundPoller.java
@@ -2,14 +2,52 @@ package se.kry.codetest;
 
 import io.vertx.core.Future;
 
+import java.awt.datatransfer.SystemFlavorMap;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+
+import static java.net.HttpURLConnection.*;
 
 public class BackgroundPoller {
 
   public Future<List<String>> pollServices(Map<String, String> services) {
     //TODO
+    services.forEach((key, value) -> services.put(key, pollService(key)));
     return Future.failedFuture("TODO");
+  }
+
+  private String pollService(String key) {
+
+    String status;
+    try {
+      URL url = new URL (key);
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setConnectTimeout(1000);
+      conn.connect();
+
+      switch (conn.getResponseCode()) {
+        case HTTP_OK:
+          status = "** HTTP OK **";
+          break;
+        case HTTP_BAD_GATEWAY:
+          status = "** HTTP_BAD_GATEWAY **";
+          break;
+        case HTTP_GATEWAY_TIMEOUT:
+          status = "** HTTP_GATEWAY_TIMEOUT **";
+          break;
+        case HTTP_NOT_FOUND:
+          status = "** HTTP_NOT_FOUND **";
+          break;
+        default:
+          status = "** Connection Error **";
+      }
+    } catch (IOException e) {
+      status = "** IOException Error **";
+    }
+    return status;
   }
 }
--- src/main/java/se/kry/codetest/MainVerticle.java
+++ src/main/java/se/kry/codetest/MainVerticle.java
@@ -4,17 +4,24 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MainVerticle extends AbstractVerticle {
 
-  private HashMap<String, String> services = new HashMap<>();
+  private HashMap<String, String> servicesMap = new HashMap<>();
   //TODO use this
   private DBConnector connector;
   private BackgroundPoller poller = new BackgroundPoller();
@@ -22,10 +29,17 @@ public class MainVerticle extends AbstractVerticle {
   @Override
   public void start(Future<Void> startFuture) {
     connector = new DBConnector(vertx);
+
     Router router = Router.router(vertx);
     router.route().handler(BodyHandler.create());
-    services.put("https://www.kry.se", "UNKNOWN");
-    vertx.setPeriodic(1000 * 60, timerId -> poller.pollServices(services));
+
+    servicesMap.put("https://www.kry.se", "UNKNOWN");
+    loadServiceFromDB(servicesMap);
+
+    vertx.setPeriodic(1000 * 30, timerId -> {
+      poller.pollServices(servicesMap);
+    });
+
     setRoutes(router);
     vertx
         .createHttpServer()
@@ -41,9 +55,12 @@ public class MainVerticle extends AbstractVerticle {
   }
 
   private void setRoutes(Router router){
+    // note *
     router.route("/*").handler(StaticHandler.create());
+
     router.get("/service").handler(req -> {
-      List<JsonObject> jsonServices = services
+
+     List<JsonObject> jsonServices = servicesMap
           .entrySet()
           .stream()
           .map(service ->
@@ -55,12 +72,68 @@ public class MainVerticle extends AbstractVerticle {
           .putHeader("content-type", "application/json")
           .end(new JsonArray(jsonServices).encode());
     });
+
     router.post("/service").handler(req -> {
       JsonObject jsonBody = req.getBodyAsJson();
-      services.put(jsonBody.getString("url"), "UNKNOWN");
+      String action = jsonBody.getString("action");
+      String url = jsonBody.getString("url");
+      String name = jsonBody.getString("name");
+      Date date = Calendar.getInstance().getTime();
+      DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+      String strDate = dateFormat.format(date);
+      if (action.equals("add"))
+      {
+        insertServiceToDB(url, name, strDate);
+        servicesMap.put(jsonBody.getString("url"), "UNKNOWN");
+      } else
+      {
+        deleteServiceFromDB(name);
+//        servicesMap.remove(jsonBody.getString("url"), "UNKNOWN");
+      }
       req.response()
           .putHeader("content-type", "text/plain")
           .end("OK");
+    });
+  }
+
+  private void loadServiceFromDB(HashMap<String, String> servicesMap){
+    connector.query("SELECT * FROM serviceDB").setHandler(res -> {
+      if (res.succeeded()) {
+//        System.out.println(res.result().getResults());
+        System.out.println("Service successfully retrieved from DB");
+        List<JsonArray> service = res.result().getResults();
+        int  i = 0;
+        for (JsonArray s: service){
+          servicesMap.put(s.getString(0), "UNKNOWN");
+          System.out.println("ServiceDB entry[" + i++ + "]: " + s);
+        }
+      } else {
+        System.out.println("Failed to load data from ServiceDB or DB does not exist.");
+      }
+    });
+  }
+
+  private void insertServiceToDB(String url, String name, String timeStamp) {
+    JsonArray param = new JsonArray();
+    param.add(url).add(name).add(timeStamp);
+    connector.query("INSERT INTO serviceDB (url, name, timeStamp) VALUES (?,?,?)", param).setHandler(res -> {
+      if (res.succeeded()) {
+        System.out.println("Service successfully inserted into DB");
+      } else {
+        System.out.println("Failed to insert service to DB");
+      }
+    });
+  }
+
+  private void deleteServiceFromDB(String name) {
+    JsonArray param = new JsonArray();
+    param.add(name);
+    connector.query("DELETE FROM serviceDB WHERE name=?", param).setHandler(res -> {
+      if (res.succeeded()) {
+        System.out.println("Service successfully deleted from DB");
+      } else {
+        System.out.println("Failed to delete service from DB");
+      }
     });
   }
 
